a5fed8f4a7b47ab0918cecfd463ec0c3e99cafb0
Rômulo Oliveira <romuloffall@gmail.com>
2022-04-29 21:43:06 -0300

    beta commit

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,13 +8,40 @@ const About = () => {
       <h1>
         Sobre o Romin's <span>Blog</span>
       </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
-        perferendis est eius tenetur fugit magni explicabo quia dolores
-        exercitationem, quasi natus voluptates repellendus dolore tempora
-        tempore aut soluta expedita ut.
-      </p>
-      <Link to="/posts/create" className="btn">Crie uma postagem</Link>
+      <div>
+        <p>
+          Prazer, me chamo Rômulo, sou desenvolvedor fullstack focado em
+          front-end.
+        </p>
+        <p>
+          Extremamente motivado, em constante desenvolvimento de minhas
+          habilidades e crescimento profissional. Confio em minhas habilidades e
+          desejo seguir carreira como desenvolvedor resolvendo problemas através
+          da tecnologia.
+        </p>
+        <p>
+          Atualmente estou cursando analise e desenvolvimentos de sistemas, e
+          trabalhando como desenvolvedor freelancer, desenvolvendo sistemas web.
+        </p>
+        <h2>Skills:</h2>
+        <h3>
+          HTML5 <br /> CSS3, SASS, Styled Components, Bulma
+          <br /> JavaScript - ES6 and TypeScript
+          <br /> Node.JS
+        </h3>
+        <h3>
+          Frameworks:<br /> React.js and React Native
+          <br /> Vue.JS
+          <br /> Bootstrap
+        </h3>
+        <h3>
+          Base de dados:<br /> MongoDB,
+          <br /> Firebase
+        </h3>
+      </div>
+      <Link to="/posts/create" className="btn">
+        Crie uma postagem
+      </Link>
     </div>
   );
 };
